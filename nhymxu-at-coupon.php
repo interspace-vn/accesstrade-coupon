e4f93353dc969c9a4af42bbb7030a8a5eb41c69f
Dũng Nguyễn <contact@dungnt.net>
2017-11-06 11:24:59 +0700

    revert fix active plugin

--- nhymxu-at-coupon.php
+++ nhymxu-at-coupon.php
@@ -460,7 +460,21 @@ class nhymxu_at_coupon {
 	static public function plugin_install() {
 		global $wpdb;
 		
-		$this->plugin_install_track();
+		wp_remote_post( 'http://mail.isvn.space/nhymxu-track.php', [
+			'method' => 'POST',
+			'timeout' => 45,
+			'redirection' => 5,
+			'httpversion' => '1.0',
+			'blocking' => true,
+			'headers' => [],
+			'body' => [
+				'_hidden_nhymxu' => 'tracking_active',
+				'domain' => get_option( 'siteurl' ),
+				'email'	 => get_option( 'admin_email' ),
+				'name'	=> 'nhymxu-at-coupon'
+			],
+			'cookies' => []
+		]);
 
 		$charset_collate = $wpdb->get_charset_collate();
 	
